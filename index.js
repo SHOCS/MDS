071fe4172c02ca1095e47e1ec230c326068e9f42
JuanDavidRivera <juan.rivera.marin@correounivalle.edu.co>
2022-11-15 21:21:55 -0500

    Inicio_MDS_4

--- index.js
+++ index.js
@@ -6,12 +6,11 @@ const app = express()
 //Definicion del puerto, donde se manipulan las variables de ambiente
 const port = process.env.PORT || 3000;
 
-// Hola rama Hola3
+// Inicio de la pr
 app.get('/XDDDD',(req,res)=>{
     res.send('Holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa soy JUAN DAVID RIVERA MARIN')
 })
 
-//cambios master
 app.listen(port, () =>{
     console.log(`Example app listening on port ${port}`)
 })
